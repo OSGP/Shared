69b09f556037295208d7882e4cee48221d70c461
Rob van Maris <commit@robvanmaris.nl>
2018-10-04 14:05:40 +0200

    OC-445: Group organisation ID, device ID and correlation UUID in a CorrelationIds object.

---
+++ shared/src/main/java/org/opensmartgridplatform/shared/domain/CorrelationIds.java
@@ -0,0 +1,32 @@
+package org.opensmartgridplatform.shared.domain;
+
+/**
+ * Set of ID's used to track things like device operations passing through the platform,
+ * i.e. the organisation identification, the device identification and the Thcorrelation UID.
+ */
+public class CorrelationIds {
+    private final String organisationIdentification;
+    private final String deviceIdentification;
+    private final String correlationUid;
+
+    public CorrelationIds(final String organisationIdentification, final String deviceIdentification,
+            final String correlationUid) {
+        this.organisationIdentification = organisationIdentification;
+        this.deviceIdentification = deviceIdentification;
+        this.correlationUid = correlationUid;
+    }
+
+    public String getOrganisationIdentification() {
+        return organisationIdentification;
+    }
+
+    public String getDeviceIdentification() {
+        return deviceIdentification;
+    }
+
+    public String getCorrelationUid() {
+        return correlationUid;
+    }
+
+
+}
